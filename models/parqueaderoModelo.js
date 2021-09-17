e738f01800d901c9d7b95f4652b986198a2e1fe2
Neider <jhonacevedocardenas@iesantaelena.edu.co>
2021-09-17 09:11:02 -0500

    Initial commit

---
+++ models/parqueaderoModelo.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+const baseDatos=require('../dataBase/conexion.js');
+
+const Parqueadero=baseDatos.define('Parqueadero',{
+
+    totales:{
+        type:DataTypes.INTEGER
+    },
+    disponibles:{
+        type:DataTypes.INTEGER
+    },
+    reservados:{
+        type:DataTypes.INTEGER
+    }
+
+
+})
+
+module.exports=Parqueadero
+
